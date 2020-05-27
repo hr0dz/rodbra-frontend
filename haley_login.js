b803f8906b8b00dc4732f5430dc6e129984c2d06
github username <github email address>
2020-05-27 08:51:20 -0500

    a whole mess

--- haley_login.js
+++ haley_login.js
@@ -1,5 +1,73 @@
-const loginForm = document.querySelector('form')
+const usersURL = "http://localhost:3000/users"
 
-function loginListener(event) {
-    loginForm.addEventListener('submit', fu)
-}
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+//const loginErrorMsg = document.getElementById("login-error-msg");
+
+
+
+function fetchUsers() {
+    fetch(usersURL)
+      .then(resp => resp.json())
+      .then(users => users.forEach(renderUser))
+  }
+
+function loginListener(){
+      loginForm.addEventListener('submit', function(event){
+      event.preventDefault()
+      const loginInput = loginForm.username.value
+      loginForm.reset()
+      addUser(event)
+    })
+  }
+
+  function addUser(event){
+    const username = event.target.dataset['username']
+  
+    const reqObj = {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:  JSON.stringify({ username: username })
+    }
+  
+    fetch(usersURL, reqObj)
+      .then(resp => resp.json())
+      .then(data => {console.log(data)})
+  }
+
+
+
+// function loginListener(){
+//     const loginForm = document.querySelector('form')
+//     loginForm.addEventListener('submit', function(event){
+//       event.preventDefault()
+//       const loginInput = event.target.login.value
+//       loginForm.reset()
+//       addUser(username)
+//     })
+//   }
+
+//   function addUser(username){
+//     const username = event.target.dataset['username']
+//     const fetchObj = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//       },
+//       body: JSON.stringify({
+//         username: username
+//       })
+//     }
+  
+//     fetch(usersURL, fetchObj)
+//     .then(resp => resp.json())
+//     .then(data => {console.log(data)})
+//   }
+
+
+//   function renderUser{
+
+//   }
