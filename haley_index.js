d11395c01a3b7e107462a5d9998e7a584504addd
github username <github email address>
2020-05-29 09:27:41 -0500

    weeeeeee

--- haley_index.js
+++ haley_index.js
@@ -163,14 +163,14 @@ function handleFollow(event){
         ) 
 };
 
-let main = document.querySelector("#main-holder")
-console.log(main)
+// let main = document.querySelector("#main-holder")
+// console.log(main)
 
-let nav = document.querySelector("#search")
-nav.addEventListener('click', function(event){
-    main.innerHTML = ''
-    main.innerHTML = "hi"
-    console.log(event)
+// let nav = document.querySelector("#search")
+// nav.addEventListener('click', function(event){
+//     main.innerHTML = ''
+//     main.innerHTML = "hi"
+//     console.log(event)
 //     ` <div class="container">
 // //     <h1>&#x2728; Search For User &#x2728;</h1>
 // //     <div id="searchWrapper">
@@ -183,7 +183,7 @@ nav.addEventListener('click', function(event){
 // //     </div>
 // //     <ul id="usersList"></ul>
 // // </div> `
-})
+// })
 
 
 
